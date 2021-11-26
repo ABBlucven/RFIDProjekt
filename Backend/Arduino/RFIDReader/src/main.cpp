7f265c29f2dd65deee07dc6eb7b96e6107e8d9c7
abbjondam <jonathan.damsgaardfalck@abbindustrigymnasium.se>
2021-11-26 11:24:48 +0100

    added arduino code

---
+++ Backend/Arduino/RFIDReader/src/main.cpp
@@ -0,0 +1,139 @@
+#include <Arduino.h>
+/**
+ * Created by K. Suwatchai (Mobizt)
+ * 
+ * Email: k_suwatchai@hotmail.com
+ * 
+ * Github: https://github.com/mobizt
+ * 
+ * Copyright (c) 2021 mobizt
+ *
+*/
+
+/** This example will show how to authenticate using 
+ * the legacy token or database secret with the new APIs (using config and auth data).
+*/
+#if defined(ESP32)
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+#endif
+
+//Provide the RTDB payload printing info and other helper functions.
+#include <addons/RTDBHelper.h>
+
+//MFRC522
+#include <SPI.h>
+#include <MFRC522.h>
+
+/* 1. Define the WiFi credentials */
+#define WIFI_SSID "ABB_Indgym_Guest"
+#define WIFI_PASSWORD "Welcome2abb"
+
+/* 2. If work with RTDB, define the RTDB URL and database secret */
+#define DATABASE_URL "rfid-project-e703f-default-rtdb.europe-west1.firebasedatabase.app" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+#define DATABASE_SECRET "LQAA7kMX5asJ5fs10uZMIly00tDz4j5t7PqUAjBL"
+
+
+/*rfid */
+#define RST_PIN         2          // Configurable, see typical pin layout above D4
+#define SS_PIN          15         // Configurable, see typical pin layout above
+
+ 
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+
+MFRC522::MIFARE_Key key; 
+
+// Init array that will store new NUID 
+String UID = "";
+
+/* 3. Define the Firebase Data object */
+FirebaseData fbdo;
+
+/* 4, Define the FirebaseAuth data for authentication data */
+FirebaseAuth auth;
+
+/* Define the FirebaseConfig data for config data */
+FirebaseConfig config;
+
+unsigned long dataMillis = 0;
+int count = 0;
+
+void setup()
+{
+
+    Serial.begin(115200);
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to Wi-Fi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(300);
+    }
+    Serial.println();
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();
+
+    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+    /* Assign the certificate file (optional) */
+    //config.cert.file = "/cert.cer";
+    //config.cert.file_storage = StorageType::FLASH;
+
+    /* Assign the database URL and database secret(required) */
+    config.database_url = DATABASE_URL;
+    config.signer.tokens.legacy_token = DATABASE_SECRET;
+
+    Firebase.reconnectWiFi(true);
+
+    /* Initialize the library with the Firebase authen and config */
+    Firebase.begin(&config, &auth);
+
+    //Or use legacy authenticate method
+    //Firebase.begin(DATABASE_URL, DATABASE_SECRET);
+}
+
+void loop()
+{
+    // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+
+  // Verify if the NUID has been readed
+  if ( ! rfid.PICC_ReadCardSerial())
+    return;
+
+  Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  Serial.println(rfid.PICC_GetTypeName(piccType));
+
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
+    return;
+  }
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      UID += String(rfid.uid.uidByte[i], HEX);
+    }
+    Serial.println(UID);
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+
+  if (millis() - dataMillis > 5000)
+    {
+        dataMillis = millis();
+        Serial.printf("Set int... %s\n", Firebase.setInt(fbdo, "/test/int", count++) ? "ok" : fbdo.errorReason().c_str());
+    }
+
+}
