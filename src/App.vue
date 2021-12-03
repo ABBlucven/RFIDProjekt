f71986ed72f8164ae3a06bfc663c5554067c9c03
BBA-ta-suniL <56075389+BBA-ta-suniL@users.noreply.github.com>
2021-12-03 10:40:32 +0100

    startsida

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,16 @@
   <div>
     <v-container>
       <header>
-        <div id="app">
-          <div id="nav">
-            <router-link to="/" class="knapp">Närvaro</router-link> |
-            <router-link to="/login" class="knapp">Login</router-link>
-          </div>
+        <p class="title">Närvarokollen</p>
+        <div id="nav">
+          <router-link
+            class="button"
+            to="/"
+          >Närvaro</router-link>
+          <router-link
+            class="button"
+            to="/login"
+          >Login</router-link>
         </div>
       </header>
     </v-container>
@@ -17,42 +22,62 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: black;
-}
-
 #nav {
-  padding: 30px;
+  text-align: right;
+  font-weight: lighter;
+  justify-content: right;
 }
 
 #nav a {
-  font-weight: bold;
   color: black;
 }
 
 #nav a.router-link-exact-active {
-  color:black;
+  color: black;
 }
 
 header {
   left: 0;
   top: 0;
-  width: 100%;
   background-color: #85b3d1ff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 30px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: black;
 }
 
 body {
   margin: 0%;
 }
 
-.knapp {
-  background-color : #b3c7d6ff;
-  padding : 10px;
-  border-radius: 25px;
+@keyframes buttons {
+  from {
+    background-color: #85b3d1ff;
+  }
+  to {
+    background-color: rgb(170, 218, 250);
+  }
+}
 
+.title {
+  font-size: 30px;
+  font-weight: bold;
+}
+
+.button {
+  padding: 15px;
+  margin: 0px 10px;
+  border-radius: 10px;
+  text-decoration: none;
+  font-size: 20px;
+}
+
+.button:hover {
+  animation-name: buttons;
+  animation-duration: 0.8s;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,50 @@
 <template>
   <div class="home">
-    <h1>Närvarande elever</h1>
     <div class="container">
+
       <div
-        class="studentCard"
-        v-for="elev in elever"
-        :key="elev"
+        class="flip-card"
+        v-for="student in presentStudents"
+        :key="student"
       >
-        <p class="name">{{elev}}</p>
-        <p>{{currentTime()}}</p>
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <p class="header">{{student.firstName}}</p>
+            <p class="subheader2">Checkade in</p>
+            <p class="bodyText">{{currentTime()}}</p>
+          </div>
+          <div class="flip-card-back">
+            <p class="subheader1">{{student.firstName}} {{student.lastName}}</p>
+            <p class="subheader2">{{student.class}}</p>
+            <p class="bodyText">Tidig 5 min</p>
+          </div>
+        </div>
       </div>
+
+      <div
+        class="flip-card"
+        v-for="student in absentStudents"
+        :key="student"
+      >
+        <div class="flip-card-inner">
+          <div
+            class="flip-card-front"
+            style="background-color: grey"
+          >
+            <p class="header">{{student.firstName}}</p>
+            <p class="subheader">{{currentTime()}}</p>
+          </div>
+          <div
+            class="flip-card-back"
+            style="background-color: grey"
+          >
+            <p class="subheader1">{{student.firstName}} {{student.lastName}}</p>
+            <p class="subheader2">{{student.class}}</p>
+            <p class="bodyText">Ej närvarande</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -25,16 +60,25 @@ export default {
   },
   data () {
     return {
-      elever: ['Linus', 'Jonathan', 'Lucas', 'Arta', 'Edvin']
+      presentStudents: [
+        { firstName: 'Jonathan', lastName: 'Damsgaard Falck', class: "190S" },
+        { firstName: 'Linus', lastName: 'Olofsson', class: "190S" },
+        { firstName: 'Lucas', lastName: 'Venlock', class: "190S" }
+      ],
+      absentStudents: [
+        { firstName: 'Jonathan', lastName: 'Damsgaard Falck', class: "190S" },
+        { firstName: 'Jonathan', lastName: 'Damsgaard Falck', class: "190S" },
+        { firstName: 'Jonathan', lastName: 'Damsgaard Falck', class: "190S" }
+      ],
     }
   },
   methods: {
-  currentTime() {
-    const current = new Date();
-    const time = current.getHours() + ":" + current.getMinutes();
-    const Time = time;
-    return Time
-  },
+    currentTime () {
+      const current = new Date()
+      const time = current.getHours() + ":" + current.getMinutes()
+      const Time = time
+      return Time
+    },
   }
 }
 </script>
@@ -42,19 +86,80 @@ export default {
 <style scoped>
 .home {
   text-align: center;
+  font-family: Arial, sans-serif;
 }
+
 .container {
   display: flex;
+  flex-wrap: wrap;
   justify-content: center;
+  padding: 30px;
 }
-.studentCard {
-  width: 200px;
-  height: 150px;
-  background-color: cadetblue;
-  padding: 20px;
-  margin: 20px;
-}
-.name {
+
+.header {
   font-size: 30px;
+  font-weight: bold;
+}
+
+.subheader1 {
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.subheader2 {
+  font-size: 15px;
+  font-weight: normal;
+  margin: 1px 0px;
+}
+
+.bodyText {
+  font-size: 10px;
+  font-weight: lighter;
+  margin: 1px 0px;
+}
+
+.flip-card {
+  background-color: transparent;
+  min-width: 200px;
+  height: 150px;
+  margin: 20px;
+  /*box-shadow: 8px 8px 20px grey;*/
+  perspective: 1000px;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+  box-shadow: 8px 8px 20px grey;
+}
+
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+  box-shadow: none;
+}
+
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+}
+
+.flip-card-front {
+  background-color: rgb(174, 225, 255);
+  color: black;
+}
+
+.flip-card-back {
+  background-color: rgb(138, 211, 253);
+  color: rgb(0, 0, 0);
+  transform: rotateY(180deg);
+  font-size: 20px;
 }
 </style>
