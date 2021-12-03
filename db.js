45782efee60aa5a24fdb39a6e348500d32a574e4
Lucas <Lucas.venlock@abbindustrigymnasium.se>
2021-12-03 10:38:17 +0100

    Delete db.js

--- db.js
+++
@@ -1,7 +0,0 @@
-// Get a RTDB instance
-import firebase from 'firebase/app'
-import 'firebase/database'
-
-export const db = firebase
-  .initializeApp({ databaseURL: 'MY PROJECT URL' })
-  .database()
