0c933dd3af3ddd7d1681a274c4aa6325c65b309f
BBA-ta-suniL <56075389+BBA-ta-suniL@users.noreply.github.com>
2021-12-09 09:36:49 +0100

    teacher page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -15,7 +15,7 @@
             class="card login"
             v-bind:class="{ error: emptyFields }"
           >
-            <h1>Sign In</h1>
+            <h1>Logga in</h1>
             <form class="form-group">
               <input
                 v-model="emailLogin"
@@ -28,14 +28,14 @@
                 v-model="passwordLogin"
                 type="password"
                 class="form-control"
-                placeholder="Password"
+                placeholder="Lösenord"
                 required
               >
               <button
                 type="submit"
                 class="btn"
                 @click="doLogin"
-              >Log in</button>
+              >Logga in</button>
 
               <!-- <p>Don't have an account? <a
                   href="#"
@@ -44,6 +44,11 @@
               </p>
               <p><a href="#">Forgot your password?</a></p> -->
             </form>
+
+            <router-link
+              class="button"
+              to="/teacherPage"
+            >Närvaro</router-link>
           </div>
 
           <!-- <div
---
+++ src/views/teacherPage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <router-link
+      class="classCard"
+      v-for="Class in classes"
+      :key="Class"
+      :to="`/${Class}`"
+    >
+      <p>
+        {{ Class }}
+      </p>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import { db } from '../db'
+
+export default {
+
+  data () {
+    return {
+      classes: ['190S', '1901', '1902', '1903', '200S', '2001', '2002', '2003', '210S', '2101', '2102', '2103']
+    }
+  },
+  firebase: {
+    documents: db.ref('students'),
+  },
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 50px;
+  justify-content: center;
+  align-items: center;
+  font-family: Arial, sans-serif;
+}
+.classCard {
+  min-width: 200px;
+  min-height: 100px;
+  background-color: rgb(138, 211, 253);
+  padding: 20px;
+  margin: 30px;
+  text-align: center;
+  box-shadow: 5px 5px 20px grey;
+  text-decoration: none;
+  color: black;
+  font-size: 50px;
+  font-weight: bold;
+}
+</style>
